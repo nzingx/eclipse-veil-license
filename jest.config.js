d3c59bbba4952a1e9ded3b40cdcf88118234f19c
nzingx <nzingx@gmail.com>
2025-06-06 01:29:10 +0700

    chore(config): update project config for release

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+// jest.config.js
+export default {
+  transform: {},
+  testEnvironment: 'node',
+  coverageDirectory: 'coverage',
+  collectCoverage: true,
+  collectCoverageFrom: ['lib/**/*.js'],
+};
